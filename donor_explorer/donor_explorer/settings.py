4fc9bc84daa51fa2becae79d6aab454c5940814a
Jason Toups <jason.p.toups@gmail.com>
2018-05-31 10:40:48 -0400

    revert to heroku deployment settings

--- donor_explorer/donor_explorer/settings.py
+++ donor_explorer/donor_explorer/settings.py
@@ -23,12 +23,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = config('SECRET_KEY')
-SECRET_KEY = 'f0uf_o(rju%d0nmy4nn=0w6!t303gqwq(k_nhv^ue7njgd9&i%'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = config('DEBUG', default=False, cast=bool)
-DEBUG = False
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = ['*']
 
@@ -83,21 +81,21 @@ WSGI_APPLICATION = 'donor_explorer.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': dj_database_url.config(
-#         default=config('DATABASE_URL')
-#     )
-# }
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'donor_explorer',
-        'USER': 'beep',
-        'PASSWORD': 'beepbeep',
-        'HOST': 'localhost'
-    }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'donor_explorer',
+#         'USER': 'beep',
+#         'PASSWORD': 'beepbeep',
+#         'HOST': 'localhost'
+#     }
+# }
 
 
 REST_FRAMEWORK = {
